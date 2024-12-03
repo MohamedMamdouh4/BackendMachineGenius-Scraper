5a168fb7fc0f4baae812abaab03ace6663ebe0eb
unknown <mohamedmamdouhgeeklab@gmail.com>
2024-12-03 16:46:29 +0200

    updates

--- Scrapers/STP/Canada/Collect-STPScrapers.js
+++ Scrapers/STP/Canada/Collect-STPScrapers.js
@@ -1,133 +1,149 @@
-const puppeteer = require('puppeteer');
-const cbcScrape = require('./CBC-Scraper');
-const thestarScraper = require('./TheStar-Scraper');
-const globalnewsScraper = require('./Globalnews-Scraper');
+const puppeteer = require("puppeteer");
+const cbcScrape = require("./CBC-Scraper");
+const thestarScraper = require("./TheStar-Scraper");
+const globalnewsScraper = require("./Globalnews-Scraper");
 
 const scrapeCBC = async () => {
+  try {
+    console.log("Start new puppeteer");
     const browser = await puppeteer.launch({
-        headless: true,
-        args: [
-            '--disable-http2',
-            '--no-sandbox',
-            '--disable-setuid-sandbox',
-            '--enable-features=NetworkService,NetworkServiceInProcess',
-        ]
+      headless: true,
+      args: [
+        "--disable-http2",
+        "--no-sandbox",
+        "--disable-setuid-sandbox",
+        "--enable-features=NetworkService,NetworkServiceInProcess",
+      ],
     });
+    console.log("======== Make new Page =======");
     const page = await browser.newPage();
-    await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36');
+    console.log("======== Navigate to CBC =======");
+    await page.setUserAgent(
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+    );
+    await page.goto("https://machinegenius.io");
+    console.log("===================== After line 20 =======================");
 
-    
-    try {
-        console.log(`Scrape the links`);
-        const URLs = await cbcScrape.scrapeURLs(page);
-        const allContent = [];
-        for (let { href, title } of URLs) {
-            console.log(`Scraping content from: ${href}`);
-            try 
-            {
-                const content = await cbcScrape.scrapeContentFromURL(page, href);
-                if(title && content && content!== "Error fetching content" )
-                {
-                    allContent.push({ url: href, title, content });
-                }
-            } catch (error) {
-                console.error(`Error scraping content from ${href}:`, error);
-                allContent.push({ url: href, title, content: 'Error fetching content' });
-            }
-        }
-        return allContent;
-    } catch (error) {
-        console.error('Error scraping all content:', error);
-        throw error;
-    } finally {
-        await browser.close();
-    }
+    // try {
+    //     console.log(`Scrape the links`);
+    //     const URLs = await cbcScrape.scrapeURLs(page);
+    //     const allContent = [];
+    //     for (let { href, title } of URLs) {
+    //         console.log(`Scraping content from: ${href}`);
+    //         try
+    //         {
+    //             const content = await cbcScrape.scrapeContentFromURL(page, href);
+    //             if(title && content && content!== "Error fetching content" )
+    //             {
+    //                 allContent.push({ url: href, title, content });
+    //             }
+    //         } catch (error) {
+    //             console.error(`Error scraping content from ${href}:`, error);
+    //             allContent.push({ url: href, title, content: 'Error fetching content' });
+    //         }
+    //     }
+    //     return allContent;
+  } catch (error) {
+    console.error("Error scraping all content:", error);
+    throw error;
+  } finally {
+    await browser.close();
+  }
 };
 
 const scrapeTheStar = async () => {
-    const browser = await puppeteer.launch({
-        headless: true,
-        args: [
-            '--disable-http2',
-            '--no-sandbox',
-            '--disable-setuid-sandbox',
-            '--enable-features=NetworkService,NetworkServiceInProcess',
-        ]
-    });
-    const page = await browser.newPage();
-    await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36');
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: [
+      "--disable-http2",
+      "--no-sandbox",
+      "--disable-setuid-sandbox",
+      "--enable-features=NetworkService,NetworkServiceInProcess",
+    ],
+  });
+  const page = await browser.newPage();
+  await page.setUserAgent(
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+  );
 
-    try {
-        const URLs = await thestarScraper.scrapeURLs(page);
-        const allContent = [];
-        for (let { href, title } of URLs) {
-            console.log(`Scraping content from: ${href}`);
-            try 
-            {
-                const content = await thestarScraper.scrapeContentFromURL(page, href);
-                if(title && content && content!== "Error fetching content" )
-                {
-                    allContent.push({ url: href, title, content });
-                }
-            } catch (error) {
-                console.error(`Error scraping content from ${href}:`, error);
-                allContent.push({ url: href, title, content: 'Error fetching content' });
-            }
+  try {
+    const URLs = await thestarScraper.scrapeURLs(page);
+    const allContent = [];
+    for (let { href, title } of URLs) {
+      console.log(`Scraping content from: ${href}`);
+      try {
+        const content = await thestarScraper.scrapeContentFromURL(page, href);
+        if (title && content && content !== "Error fetching content") {
+          allContent.push({ url: href, title, content });
         }
-        return allContent;
-    } catch (error) {
-        console.error('Error scraping all content:', error);
-        throw error;
-    } finally {
-        await browser.close();
+      } catch (error) {
+        console.error(`Error scraping content from ${href}:`, error);
+        allContent.push({
+          url: href,
+          title,
+          content: "Error fetching content",
+        });
+      }
     }
+    return allContent;
+  } catch (error) {
+    console.error("Error scraping all content:", error);
+    throw error;
+  } finally {
+    await browser.close();
+  }
 };
 
 const scrapeGlobalnews = async () => {
-    const browser = await puppeteer.launch({
-        headless: true,
-        args: [
-            '--disable-http2',
-            '--no-sandbox',
-            '--disable-setuid-sandbox',
-            '--enable-features=NetworkService,NetworkServiceInProcess',
-        ]
-    });
-    const page = await browser.newPage();
-    await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36');
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: [
+      "--disable-http2",
+      "--no-sandbox",
+      "--disable-setuid-sandbox",
+      "--enable-features=NetworkService,NetworkServiceInProcess",
+    ],
+  });
+  const page = await browser.newPage();
+  await page.setUserAgent(
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+  );
 
-    try {
-        const URLs = await globalnewsScraper.scrapeURLs(page);
-        const allContent = [];
-        for (let { href, title } of URLs) {
-            console.log(`Scraping content from: ${href}`);
-            try 
-            {
-                const content = await globalnewsScraper.scrapeContentFromURL(page, href);
-                if(title && content)
-                {
-                        console.log({url:href})
-                        allContent.push({ url: href, title, content });
-                        var filteredAllContent = allContent.filter((url) => url != null);
-                }
-            } catch (error) {
-                console.error(`Error scraping content from ${href}:`, error);
-                filteredAllContent.push({ url: href, title, content: 'Error fetching content' });
-            }
+  try {
+    const URLs = await globalnewsScraper.scrapeURLs(page);
+    const allContent = [];
+    for (let { href, title } of URLs) {
+      console.log(`Scraping content from: ${href}`);
+      try {
+        const content = await globalnewsScraper.scrapeContentFromURL(
+          page,
+          href
+        );
+        if (title && content) {
+          console.log({ url: href });
+          allContent.push({ url: href, title, content });
+          var filteredAllContent = allContent.filter((url) => url != null);
         }
-        return filteredAllContent;
-    } catch (error) {
-        console.error('Error scraping all content:', error);
-        throw error;
-    } finally {
-        await browser.close();
+      } catch (error) {
+        console.error(`Error scraping content from ${href}:`, error);
+        filteredAllContent.push({
+          url: href,
+          title,
+          content: "Error fetching content",
+        });
+      }
     }
+    return filteredAllContent;
+  } catch (error) {
+    console.error("Error scraping all content:", error);
+    throw error;
+  } finally {
+    await browser.close();
+  }
 };
 
-
 module.exports = {
-    scrapeCBC,
-    scrapeTheStar,
-    scrapeGlobalnews,
-
+  scrapeCBC,
+  scrapeTheStar,
+  scrapeGlobalnews,
 };
